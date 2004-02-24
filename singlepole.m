f6b94f02053abe8fd9fc3faf17129694b45d3ee1
Norton Allen <allen@huarp.harvard.edu>
2004-02-24 18:31:09 +0000

    Single Pole filter function.

---
+++ singlepole.m
@@ -0,0 +1,15 @@
+function [b,a] = singlepole( Wn, ftype );
+% [b,a] = singlepole( Wn, ftype );
+% Wn is the normalized cutoff frequency where Fnyquist = 1
+% ftype is either 'lowpass' or 'highpass'
+RC = 1/(2*pi*Wn);
+T = 1/2;
+if nargin < 2 || strcmp(ftype,'lowpass')
+  a = [ (1+2*RC/T) (1-2*RC/T) ];
+  b = [ 1 1 ];
+elseif strcmp(ftype,'highpass')
+  a = [ (2/T + 1/RC) (1/RC - 2/T) ];
+  b = [ (2/T) (-2/T) ];
+else
+  error('Unrecognized ftype');
+end
