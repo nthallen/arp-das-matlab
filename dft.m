d936b794a21d92d7b5c2a04b5e3cca469d650a5a
Norton Allen <allen@huarp.harvard.edu>
2006-01-17 16:26:44 +0000

    Optimized a special case for dft. New H2O directories for the new notebook

--- dft.m
+++ dft.m
@@ -20,11 +20,21 @@ if dx & dy & (size(X,2) ~= size(Y,2))
   error('X and Y must have the same number of columns');
 end
 F = zeros(length(f),ncols);
-for i = 0:ncols-1
-  x = X(:,i*dx+1);
-  y = Y(:,i*dy+1);
-  v = ~isnan(x)&~isnan(y);
+if dx == 0 && all(~any(isnan(X))) && all(~any(isnan(Y)))
+  % all columns of Y have the same X coordinates
+  % and there are no NaNs buried in either vector
+  fprintf(1,'Doing optimized dft\n');
   for k = 1:length(f)
-    F(k,i+1) = sum(y(v) .* exp(-j*2*pi*f(k)*x(v)));
+    F(k,:) = exp(-j*2*pi*f(k)*X)' * Y;
+  end
+else
+  fprintf(1,'Doing slow dft\n');
+  for i = 0:ncols-1
+    x = X(:,i*dx+1);
+    y = Y(:,i*dy+1);
+    v = ~isnan(x)&~isnan(y);
+    for k = 1:length(f)
+      F(k,i+1) = sum(y(v) .* exp(-j*2*pi*f(k)*x(v)));
+    end
   end
 end
