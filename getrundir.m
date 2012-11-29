c4f1019b5a07ce0096c6d64452085e932de6a498
Norton Allen <allen@huarp.harvard.edu>
2012-11-29 16:49:22 +0000

    Don't die on error

--- getrundir.m
+++ getrundir.m
@@ -1,14 +1,23 @@
-function run = getrundir;
+function run = getrundir
 run = '';
+% Special case for engineering plots: check for UserData in
+% the figure containing the callbackobject. Fails for
+% scan_viewer with a listener on slider value since the value
+% object does not have a type nor a parent.
 co = get(0,'callbackobject');
-while ~isempty(co) % && isobject(co)
-  t = [ get(co,'type') '/' get(co,'tag') ];
-  if strcmp( t, 'figure/eng_ui')
-    run = get( co, 'UserData' );
-    break;
-  end
-  co = get( co, 'Parent' );
+while ~isempty(co)
+    try
+      t = [ get(co,'type') '/' get(co,'tag') ];
+      if strcmp( t, 'figure/eng_ui')
+        run = get( co, 'UserData' );
+        break;
+      end
+      co = get( co, 'Parent' );
+    catch err
+      break;
+    end
 end
-if length(run) == 0
+% Otherwise, use the current directory
+if isempty(run)
   run = pwd;
 end
