2ca02daa3a31ebd4f6bd9d1c076903cf8058958d
Norton Allen <allen@huarp.harvard.edu>
2021-03-24 19:38:32 -0400

    Always add records that arrive

--- dfs/data_records.m
+++ dfs/data_records.m
@@ -16,24 +16,23 @@ classdef data_records < handle
             end
         end
         function process_record(drs,rec_name, str)
-          if isfield(drs.records, rec_name)
-            dr = drs.records.(rec_name);
-            dr.process_record(str);
-            if isempty(drs.max_time) || dr.max_time > drs.max_time
-                drs.max_time = dr.max_time;
-            else
-              recs = fieldnames(drs.records);
-              newmax = [];
-              for i=1:length(recs)
-                rmax = drs.records.(recs{i}).max_time;
-                if isempty(newmax) | rmax > newmax
-                  rmax = newmax;
-                end
-              end
-              drs.max_time = newmax;
-            end
+          if ~isfield(drs.records, rec_name)
+            drs.add_record(rec_name);
+          end
+          dr = drs.records.(rec_name);
+          dr.process_record(str);
+          if isempty(drs.max_time) || dr.max_time > drs.max_time
+            drs.max_time = dr.max_time;
           else
-            warning('Record "%s" not defined"', rec_name);
+            recs = fieldnames(drs.records);
+            newmax = [];
+            for i=1:length(recs)
+              rmax = drs.records.(recs{i}).max_time;
+              if isempty(newmax) | rmax > newmax
+                newmax = rmax;
+              end
+            end
+            drs.max_time = newmax;
           end
         end
     end
