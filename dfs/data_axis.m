b89115bc3f2373571f8ef042ba75f8ed76233033
Norton Allen <allen@huarp.harvard.edu>
2021-10-27 14:43:36 -0400

    data_axis.m: Whitespace

--- dfs/data_axis.m
+++ dfs/data_axis.m
@@ -13,12 +13,14 @@ classdef data_axis < handle
       da.axis = axes(dfig.fig,'visible','off');
       da.label = strrep(label,'_','\_');
     end
+    
     function n = add_line(da, rec_name, var_name)
       % returns the line index
       da.lines{end+1} = struct('rec',rec_name,'var',var_name);
       da.redraw();
       n = length(da.lines);
     end
+    
     function redraw(da)
       cla(da.axis);
       for i=1:length(da.lines)
